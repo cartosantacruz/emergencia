1df9a6b10cb1e32b8830a3d1841c61ca2efc8654
Diego Gutierrez <77216776+cartosantacruz@users.noreply.github.com>
2024-05-31 12:40:58 -0300

    Add files via upload

---
+++ data/helipuertos_9.js
@@ -0,0 +1,11 @@
+var json_helipuertos_9 = {
+"type": "FeatureCollection",
+"name": "helipuertos_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": "4", "fna": "Santa Cruz\/ Estancia San Ramón", "nam": "Estancia San Ramón", "indicador": "HSR", "tipo": "HEL", "clasificac": "PRIVADO", "provincia": "SANTA CRUZ", "coord_geog": "50° 18' 11,00'' S 069° 55' 04,00'' W", "ubicacion": "155 KM al NNW de la ciudad de RIO GALLEGOS. provincia de Santa Cruz", "contacto": "Propietario: ENAP SIPETROL ARGENTINA S.A." }, "geometry": { "type": "Point", "coordinates": [ -69.9175748, -50.30306055 ] } },
+{ "type": "Feature", "properties": { "gid": "2", "fna": "El Turbio\/  Estancia Santa Ana", "nam": "Estancia Santa Ana", "indicador": "HTS", "tipo": "HEL", "clasificac": "PRIVADO", "provincia": "SANTA CRUZ", "coord_geog": "51° 41' 09,00'' S 071° 58' 04,00'' W", "ubicacion": "9,5 KM al E de la localidad de EL TURBIO, provincia de Santa Cruz", "contacto": "Propietario: GOYAIKE S.AA.C.I.Y.F (011) 342-5257" }, "geometry": { "type": "Point", "coordinates": [ -71.96890789, -51.68642909 ] } },
+{ "type": "Feature", "properties": { "gid": "1", "fna": "Cabo Vírgenes\/BRM", "nam": "BRM Cabo Vírgenes", "indicador": "HCV", "tipo": "HEL", "clasificac": "PRIVADO", "provincia": "SANTA CRUZ", "coord_geog": "52° 19' 49,00'' S 068° 24' 30,00'' W", "ubicacion": "98 KM al SE de la ciudad de RIO GALLEGOS, provincia de Santa Cruz", "contacto": "Propietario: Leonardo Javier FERNANDEZ- Encargado: Esteban ROCCA (011) 15-64569859" }, "geometry": { "type": "Point", "coordinates": [ -68.40794733, -52.33027544 ] } },
+{ "type": "Feature", "properties": { "gid": "3", "fna": "Punta Bandera", "nam": "Punta Bandera", "indicador": "HPB", "tipo": "HEL", "clasificac": "PRIVADO", "provincia": "SANTA CRUZ", "coord_geog": "50° 18' 43,20'' S 072° 47' 32,30'' W", "ubicacion": "37 KM al W de la ciudad de EL CALAFATE, porvincia de Santa Cruz", "contacto": "Propietario: GOYAIKE S.AA.C.I.Y.F (011) 342-5257" }, "geometry": { "type": "Point", "coordinates": [ -72.794551761224284, -50.312231621765982 ] } }
+]
+}
